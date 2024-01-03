69494dfe00d5279962f4bf44a551686026c7d04b
willqa01 <willqa01@gmail.com>
2024-01-03 15:24:56 +0100

    test2

--- APIphp.php
+++ APIphp.php
@@ -28,17 +28,38 @@ if (isset($_GET['nom'])){
 if (isset($_GET['prenom'])){
     $retour->prenom = $_GET["prenom"];
 }
-// if (isset($_GET['adresse'])){
-//     $retour->adresse = $_GET["adresse"];
-//}
-if (isset($_GET['hobbies'])){
-    $retour->hobbies = $_GET["hobbies"];
+if (isset($_GET['rue'])){
+    $retour->adresse['rue'] = $_GET["rue"];
 }
-if (isset($_GET['adresse'])) {
-    $adresse = $_GET['adresse'];
-    $retour->adresse['ville'] = $adresse['ville'];
+if (isset($_GET['ville'])){
+    $retour->adresse['ville'] = $_GET["ville"];
+}
+if (isset($_GET['codePostal'])){
+    $retour->adresse['codePostal'] = $_GET["codePostal"];
 }
 
+
+// if (isset($_GET[0])){
+//     $retour->hobbies[0] = $_GET[0];
+// }
+// if (isset($_GET[1])){
+//     $retour->hobbies[1] = $_GET[1];
+// }
+if (isset($_GET["hobbies1"])){
+    $retour->hobbies[0] = $_GET["hobbies1"];
+}else {
+    $retour->hobbies[0] = "";
+}
+if (isset($_GET["hobbies2"])){
+    $retour->hobbies[1] = $_GET["hobbies2"];
+}else {
+    $retour->hobbies[1] = "";
+}
+if (isset($_GET["hobbies3"])){
+    $retour->hobbies[2] = $_GET["hobbies3"];
+}else {
+    $retour->hobbies[2] = "";
+}
 
 print_r($retour);
 echo "<br>";
