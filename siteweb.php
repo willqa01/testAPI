44253d3a89d716b8b613853540d6db9658d839a1
willqa01 <willqa01@gmail.com>
2024-01-04 12:20:14 +0100

    4midi

---
+++ siteweb.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php
+
+$url = "http://localhost/API/API2.php";
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => $url,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+$reponse = json_decode($response);
+
+foreach($reponse as $key => $value){
+    // $value->departement_id;
+    // $value["nom_departement"];
+?>
+
+<H1><?php echo $value->nom_departement?></H1>
+<H2><?php echo $value->emplacement?></H2>
+<br>
+<br>
+<?php
+
+}
+
+?>
